a23a0e980a56898097980ba559d22f06a6a20a71
Isaac Pérez <ipesanz@gmail.com>
2020-10-27 12:10:19 -0400

    ADD : Levenshtein method to metrics

--- week3/metrics.py
+++ week3/metrics.py
@@ -40,6 +40,39 @@ def js_div(m1, m2):
     
     return np.array(result)
 
+# Comparing two strings by counting minimum number of operations
+# required to transform one string to another
+# seq1 & seq2 are strings to be compared
+def levensthein(seq1, seq2):
+    #SOURCE: https://stackabuse.com/levenshtein-distance-and-text-similarity-in-python/
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros ((size_x, size_y))
+    for x in range(size_x):
+        matrix [x, 0] = x
+    for y in range(size_y):
+        matrix [0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix [x,y] = min(
+                    matrix[x-1,y] + 1,
+                    matrix[x-1,y-1] + 1,
+                    matrix[x,y-1] + 1
+                )
+
+    #print (matrix)
+    # RETURNS Nº of changes required.
+    # Between "mama" and "papa" == 2
+    return (matrix[size_x - 1, size_y - 1])
+
 MEASURES = {
     "l2_dist": l2_dist,
     "l1_dist": l1_dist,
