BUG: Unable to execute : --masking 1 -kd akace -ld ORB -km BF

week4/descriptors_local.py
@@ -10,6 +10,7 @@ def compute_local_desc(image, mask, keypoint, method_name, options):
     return method(image, keypoint, mask, options)
 
 def orb_descriptor(image, kp, mask, options):
+    print("descriptors_local.py ==> orb_descriptor")
     """
     Extract descriptors from image using the ORB method.
     Args:
@@ -32,13 +33,42 @@ def orb_descriptor(image, kp, mask, options):
     descriptor = orb.compute(grayscale_image, kp)[1]
     return descriptor
 
+
+def akace_descriptor(image, kp, mask, options):
+    print("descriptors_local.py ==> akace_descriptor")
+    """
+    Extract descriptors from image using the AKACE method.
+    # REF0:
+    Args:
+        image (ndarray): (H x W) 2D array of type np.uint8 containing a grayscale image.
+        mask:
+        keypoints (list): list of cv2.KeyPoint objects.
+    Returns:
+        descriptors (ndarray): 2D array of type np.float32 and shape (#keypoints x 128)
+            containing local descriptors for the keypoints.
+    """
+
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    grayscale_image = cv2.resize(grayscale_image, (256, 256), interpolation=cv2.INTER_AREA)
+
+    if mask is not None:
+        mask = cv2.resize(mask, (256, 256), interpolation=cv2.INTER_AREA)
+        mask = (mask==0).astype(np.uint8)*255
+
+    orb = cv2.ORB_create(WTA_K=options.orb_wtak)
+    descriptor = orb.compute(grayscale_image, kp)[1]
+    return descriptor
+
 # Selection utils
 ORB = "ORB"
-OPTIONS = [ORB]
+AKA = "akace"
+OPTIONS = [ORB, AKA]
 
 METHOD_MAPPING = {
-    OPTIONS[0]: orb_descriptor
+    OPTIONS[0]: orb_descriptor,
+    OPTIONS[1]: akace_descriptor
 }
 
 def get_method(method):
-    return METHOD_MAPPING[method]
+    result = METHOD_MAPPING[method]
+    return result

week4/match_paintings_local_desc.py
@@ -195,9 +195,10 @@ def match_paintings(args):
         qs_gts = np.array(qs_gts).reshape(-1, 1)        
         if qs_gts[0].dtype == 'O':
             qs_gts = np.concatenate([[q for q in qs_gt[0]] for qs_gt in qs_gts]).reshape(-1, 1)
-        
-        # Compute F1 for painting included/not included
-        show_f1_scores(assignments, num_matches, qs_gts, max_matches = 50)
+
+        if isDebug():
+            # Compute F1 for painting included/not included
+            show_f1_scores(assignments, num_matches, qs_gts, max_matches = 50)
 
         map_at_1 = mapk(qs_gts, assignments, k=1)
         map_at_5 = mapk(qs_gts, assignments, k=5)