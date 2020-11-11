413817fdc8431e8d3ea9a0774ca23fcd43ff54d7
SoftonCV <sergidipi@gmail.com>
2020-11-11 17:26:23 +0100

    One more empty function.

--- week5/clustering_methods.py
+++ week5/clustering_methods.py
@@ -5,19 +5,24 @@ from debug_utils import *
 def k_means():
     pass
 
-def BOVW():
+def shift_mean():
+    pass
+
+def bovw():
     pass
 
 
 
 # Selection utils
 KMEANS = "KM"
-BOW = "BOVW"
-OPTIONS = [KMEANS, BOVW,]
+BOVW = "BOVW"
+SHIFT_MEAN = "SHIFT_MEAN"
+OPTIONS = [KMEANS, SHIFT_MEAN, BOVW]
 
 METHOD_CLUSTERING = {
     OPTIONS[0]: k_means,
-    OPTIONS[1]: BOVW
+    OPTIONS[1]: shift_mean,
+    OPTIONS[2]: bovw,
 }
 
 def get_method(method):
